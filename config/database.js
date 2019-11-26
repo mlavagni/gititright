f26b20f781ff113a8a4d81617716703d5b403822
mark <mark@marks-MacBook-Pro.local>
2019-11-26 12:50:54 -0600

    added atlas db connectec

---
+++ config/database.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
+
+
+  // database connection event
+mongoose.connection.on('connected', function () {
+    console.log(`Mongoose connected`);
+  });
+  
+  module.exports = mongoose;
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// load the env vars
+require('dotenv').config();
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+require('./config/database');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
