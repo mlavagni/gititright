4c7125c82bb7cfac1a2aa2e1efa77446f7e638a3
mark <mark@marks-MBP.lan1>
2019-12-01 21:54:36 -0600

    added create reminder

--- controllers/reminders.js
+++ controllers/reminders.js
@@ -2,9 +2,20 @@ const User = require('../models/user');
 
 module.exports = {
  index,
+ create
   };
 
 
+  function create(req, res, next) {
+      console.log('entro al create')
+    User.findById(req.session.passport.user).exec(function(err, user) {
+        user.reminders.push(req.body);
+        user.save (function(err) {
+            if (err) return next(err); 
+            res.render('reminders/index', {user});
+        })
+    })
+}
 
 function index(req, res, next) {
     User.findById(req.session.passport.user).exec(function(err, user) {
--- routes/reminders.js
+++ routes/reminders.js
@@ -3,6 +3,7 @@ var remindersCtrl = require('../controllers/reminders');
 
 // GET / reminders
 router.get('/reminders',remindersCtrl.index);
+router.post('/users/:id/reminders',remindersCtrl.create);
 
 function isLoggedIn(req, res, next) {
     if ( req.isAuthenticated() ) return next();
