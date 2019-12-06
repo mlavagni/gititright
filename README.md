fdb62d2bb605d2654b023da6ff748da30d24ef2e
mark <mark@marks-MacBook-Pro.local>
2019-12-06 11:52:01 -0600

    refactored reademe file and javascript file

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ The purpose of the app is to have information on your friends and families, what
 
 
 # User Stories
-1- As a user I would like to login with my google account  
-2- As a user when I log in the first time I would like to to have my profile created
-3- As a user I would like to Add, Search, Remove and Update Sizes in my profile
-4- As a user I would like to Add, search, remove and update articles in to my wishlist in my profile
-5- As a user I would like to Add, search, remove and update reminders in my profile
-6- As a user I would like to add access users so they can check my wishlist and sizes 
-7- As a user I would like to see others wishlist and sizes if I have permission
-8- As a user I would like to let others users know if I buy something from there wishlist
-9- As a user I want to be able to log out
+    1- As a user I would like to login with my google account  
+    2- As a user when I log in the first time I would like to to have my profile created
+    3- As a user I would like to Add, Search, Remove and Update Sizes in my profile
+    4- As a user I would like to Add, search, remove and update articles in to my wishlist in my profile
+    5- As a user I would like to Add, search, remove and update reminders in my profile
+    6- As a user I would like to add access users so they can check my wishlist and sizes 
+    7- As a user I would like to see others wishlist and sizes if I have permission
+    8- As a user I would like to let others users know if I buy something from there wishlist
+    9- As a user I want to be able to log out
 
 # ERD
 ![ERD](public/images/readme-imgs/erd.png)
---
+++ public/javascripts/test.js
@@ -0,0 +1,57 @@
+
+const reminderClick = document.querySelectorAll(".reminderRow")
+const sizeClick = document.querySelectorAll(".sizeRow")
+
+
+reminderClick.forEach(function (reminder){
+	reminder.addEventListener('click', function(evt){
+
+		const idEl = document.getElementById("idTextBox");
+		idEl.value = reminder.attributes.value.value
+
+		const nameEl = document.getElementById("nameTextBox");
+		nameEl.value = reminder.children[0].textContent
+
+		const dateEl = document.getElementById("dateBox");
+		
+		dateFormat =new Date(reminder.children[1].textContent)
+		
+		dateEl.value = dateFormat.getFullYear().toString() + "-" +
+		(dateFormat.getMonth() + 1).toString().padStart(2, 0) + 
+    '-' + dateFormat.getDate().toString().padStart(2, 0);
+		
+		const daysEl = document.getElementById("daysTextBox");
+		daysEl.value = reminder.children[2].textContent
+
+		//change the form action to PUT
+		document.getElementById("form-save-update").action =`/reminders/${ reminder.attributes.value.value }?_method=PUT`;
+
+	
+})
+
+})
+
+sizeClick.forEach(function (size){
+	size.addEventListener('click', function(evt){
+
+		const sidEl = document.getElementById("idSize");
+		sidEl.value = size.attributes.value.value
+
+		const snameEl = document.getElementById("nameSize");
+		snameEl.value = size.children[0].textContent
+
+		const ssizeEl = document.getElementById("sizeSize");
+		ssizeEl.value = size.children[1].textContent
+
+		const sbrandEl = document.getElementById("brandSize");
+		sbrandEl.value = size.children[2].textContent
+		
+		const sdescriptionEl = document.getElementById("descriptionSize");
+		sdescriptionEl.value = size.children[3].textContent
+
+		//change the form action to PUT
+		document.getElementById("form-save-update").action =`/sizes/${ size.attributes.value.value }?_method=PUT`;
+
+})
+
+})
